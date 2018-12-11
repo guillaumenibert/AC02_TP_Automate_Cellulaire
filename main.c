d80d31a75c1304e673ab551adf6ff8076046d6db
MCDoors <40465432+MCDoors@users.noreply.github.com>
2018-12-11 21:46:47 +0100

    alpha01_build03

--- main.c
+++ main.c
@@ -4,7 +4,17 @@
 
 int main()
 {
-    Grille Jeu = grille(10, 11, 'm');
+    int lignes = 1, colonnes = 1;
+    char mode = 'm';
+    printf("Entrez le nombre de lignes : ");
+    scanf("%d", &lignes);
+    printf("Entrez le nombre de colonnes : ");
+    scanf("%d", &colonnes);
+    printf("Entrez le mode, 'a' pour aleatoire, 'm' pour manuel : ");
+    while (getchar()!='\n'); // Vidage du buffer
+    scanf("%c", &mode);
+
+    Grille Jeu = grille(lignes, colonnes, 'm');
     affichage_grille(Jeu);
     return 0;
 }
