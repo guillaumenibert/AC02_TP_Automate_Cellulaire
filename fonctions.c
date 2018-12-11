4698a59a24acc13dc566cae3f56f8ce223d776a3
MCDoors <40465432+MCDoors@users.noreply.github.com>
2018-12-11 21:46:22 +0100

    alpha01_build03

--- fonctions.c
+++ fonctions.c
@@ -7,17 +7,18 @@ Grille grille(int nb_lignes, int nb_colonnes, char mode)
     int i, j;
     Grille matrice;
 
-    matrice.matrix = malloc(nb_lignes * sizeof(char*));
-
+    matrice.matrix = malloc(nb_lignes * sizeof(char*)); // On alloue de la mémoire pour les lignes.
     if (!matrice.matrix)
         exit(0);
 
     matrice.nb_colonnes = nb_colonnes;
     matrice.nb_lignes = nb_lignes;
 
-    for (i = 0; i < nb_colonnes; i++)
+    for (i = 0; i < nb_lignes; i++) // Parcours des i lignes.
     {
-        matrice.matrix[i] = malloc(nb_colonnes * sizeof(char));
+        matrice.matrix[i] = malloc(nb_colonnes * sizeof(char)); // Chaque ligne contient des colonnes, il faut allouer de la mémoire aux colonnes donc.
+            if (!matrice.matrix[i])
+                exit(0);
     }
 
     for (i = 0; i < nb_lignes ; i++) // Permet d'initialiser la grille avec que des ' ' pour ne pas avoir n'importe quoi.
@@ -34,9 +35,11 @@ void affichage_grille(Grille matrice)
 {
     int i = 0, j = 0;
 
-    for(i = 0; i < matrice.nb_lignes; i++)
+    printf("\n");
+
+    for(i = 0; i < matrice.nb_lignes; i++) // Pour chaque ligne, on dessine l'interface graphique
     {
-        for (j = 0; j < matrice.nb_colonnes*2+1; j++)
+        for (j = 0; j < matrice.nb_colonnes*2+1; j++) // Cette formule a été trouvée empiriquement et semble fonctionner, elle permet de dessiner les "-"
         {
             printf("-");
         }
@@ -44,15 +47,16 @@ void affichage_grille(Grille matrice)
 
         for (j = 0; j < matrice.nb_colonnes; j++)
         {
-            printf("|%c", matrice.matrix[i][j]);
+            printf("|%c", matrice.matrix[i][j]); // le %c correspond a une cellule morte si on a ' ' sinon c'est une cellule vivante.
         }
-        printf("|");
+        printf("|"); // La dernière barre pour fermer le tableau à droite.
         printf("\n");
     }
 
-    for (j = 0; j < matrice.nb_colonnes*2+1; j++)
+    for (j = 0; j < matrice.nb_colonnes*2+1; j++) // La dernière ligne "--------..." pour le bas.
         {
             printf("-");
         }
-}
 
+    printf("\n");
+}
