980a2b91b141b1ee218083a875a6175faea3a558
MCDoors <40465432+MCDoors@users.noreply.github.com>
2018-12-12 19:29:57 +0100

    alpha01_build04
    Utilisation d'un pointeur sur un objet de type Grille.

--- header.h
+++ header.h
@@ -1,10 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
 typedef struct Grille Grille;
 struct Grille
 {
-    char** matrix; // L'atribut "matrix" est la matrice représentant la grille (un tableau à deux dimensions). En l'occurence nous priviliégons l'utilisation d'un double pointeuur (un pour les lignes) (un pour les colonnes) car l'utilisateur a le choix de la taille de son tableau.
+    //premioer pointeur pointe sur les lignes
+    //ces lignes contient des colonnes donc le deuxieme pointeur pointe sur les colonnes
+    char** matrice; // L'atribut "matrice" est la matrice représentant la grille (un tableau à deux dimensions). En l'occurence nous privilégions l'utilisation d'un double pointeuur (un pour les lignes) (un pour les colonnes) car l'utilisateur a le choix de la taille de son tableau.
     int nb_lignes; // Utile pour utiliser d'autres informations (longueur entre autres) dans des fonctions.
     int nb_colonnes;
 };
 
-Grille grille(int nb_lignes, int nb_colonnes, char mode);
-void affichage_grille(Grille matrice);
+Grille* creerGrille(int nb_lignes, int nb_colonnes, char mode);
+void affichageGrille(Grille* matrice);
