0e77d847a6b22af9a1afe8ba7f2d50e49a2469e1
MCDoors <40465432+MCDoors@users.noreply.github.com>
2018-12-12 19:31:32 +0100

    alpha01_build04
    Utilisation d'un pointeur sur un objet de type Grille.

--- fonctions.c
+++ fonctions.c
@@ -1,62 +1,62 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "header.h"
+#include "fonctions.h"
 
-Grille grille(int nb_lignes, int nb_colonnes, char mode)
+Grille* creerGrille(int nb_lignes, int nb_colonnes, char mode)
 {
     int i, j;
-    Grille matrice;
+    Grille* grille = malloc(sizeof(Grille));
 
-    matrice.matrix = malloc(nb_lignes * sizeof(char*)); // On alloue de la mémoire pour les lignes.
-    if (!matrice.matrix)
+    if (!grille)
         exit(0);
 
-    matrice.nb_colonnes = nb_colonnes;
-    matrice.nb_lignes = nb_lignes;
+    (*grille).matrice = malloc(nb_lignes * sizeof(char*)); // On alloue de la mémoire pour les lignes.
+    if (!(*grille).matrice)
+        exit(0);
+
+    (*grille).nb_colonnes = nb_colonnes;
+    (*grille).nb_lignes = nb_lignes;
 
     for (i = 0; i < nb_lignes; i++) // Parcours des i lignes.
     {
-        matrice.matrix[i] = malloc(nb_colonnes * sizeof(char)); // Chaque ligne contient des colonnes, il faut allouer de la mémoire aux colonnes donc.
-            if (!matrice.matrix[i])
-                exit(0);
+        (*grille).matrice[i] = malloc(nb_colonnes * sizeof(char)); // Chaque ligne contient des colonnes, il faut allouer de la mémoire aux colonnes donc.
+        if (!(*grille).matrice[i])
+            exit(0);
     }
 
     for (i = 0; i < nb_lignes ; i++) // Permet d'initialiser la grille avec que des ' ' pour ne pas avoir n'importe quoi.
     {
         for(j = 0; j < nb_colonnes; j++)
         {
-            matrice.matrix[i][j] = ' ';
+            (*grille).matrice[i][j] = ' ';
         }
     }
-    return(matrice);
+    return(grille);
 }
 
-void affichage_grille(Grille matrice)
+void affichageGrille(Grille* matrice)
 {
     int i = 0, j = 0;
 
     printf("\n");
 
-    for(i = 0; i < matrice.nb_lignes; i++) // Pour chaque ligne, on dessine l'interface graphique
+    for(i = 0; i < (*matrice).nb_lignes; i++) // Pour chaque ligne, on dessine l'interface graphique
     {
-        for (j = 0; j < matrice.nb_colonnes*2+1; j++) // Cette formule a été trouvée empiriquement et semble fonctionner, elle permet de dessiner les "-"
+        for (j = 0; j < (*matrice).nb_colonnes*2+1; j++) // Cette formule a été trouvée empiriquement et semble fonctionner, elle permet de dessiner les "-"
         {
             printf("-");
         }
         printf("\n");
 
-        for (j = 0; j < matrice.nb_colonnes; j++)
+        for (j = 0; j < (*matrice).nb_colonnes; j++)
         {
-            printf("|%c", matrice.matrix[i][j]); // le %c correspond a une cellule morte si on a ' ' sinon c'est une cellule vivante.
+            printf("|%c", (*matrice).matrice[i][j]); // le %c correspond a une cellule morte si on a ' ' sinon c'est une cellule vivante.
         }
         printf("|"); // La dernière barre pour fermer le tableau à droite.
         printf("\n");
     }
 
-    for (j = 0; j < matrice.nb_colonnes*2+1; j++) // La dernière ligne "--------..." pour le bas.
-        {
-            printf("-");
-        }
-
+    for (j = 0; j < (*matrice).nb_colonnes*2+1; j++) // La dernière ligne "--------..." pour le bas.
+    {
+        printf("-");
+    }
     printf("\n");
 }
