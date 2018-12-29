26993aeabccdbe61596ca605418ac900283c8efa
MCDoors <40465432+MCDoors@users.noreply.github.com>
2018-12-29 14:47:46 +0100

    0.3.0

--- fonctions.h
+++ fonctions.h
@@ -1,16 +1,37 @@
+//
+// Created by Luc on 16/12/2018.
+//
+
+#ifndef AUTOMATES_CELLULAIRES_FONCTIONS_H
+#define AUTOMATES_CELLULAIRES_FONCTIONS_H
+
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 
 typedef struct Grille Grille;
 struct Grille
 {
-    //premioer pointeur pointe sur les lignes
-    //ces lignes contient des colonnes donc le deuxieme pointeur pointe sur les colonnes
-    char** matrice; // L'atribut "matrice" est la matrice représentant la grille (un tableau à deux dimensions). En l'occurence nous privilégions l'utilisation d'un double pointeuur (un pour les lignes) (un pour les colonnes) car l'utilisateur a le choix de la taille de son tableau.
-    int nb_lignes; // Utile pour utiliser d'autres informations (longueur entre autres) dans des fonctions.
+    char** pointeurCase;
+    int nb_lignes;
     int nb_colonnes;
 };
 
-Grille* creerGrille(int nb_lignes, int nb_colonnes, char mode);
-void affichageGrille(Grille* matrice);
+Grille* creerGrille(int nb_lignes, int nb_colonnes);
+void affichageGrille(Grille* grille);
+void affichageGrille2(Grille* grille);
+int random (int probabilite);
+Grille* grilleAleatoire (Grille *grille, int probabilite);
+void grilleAleatoire_v2(Grille *grille, int probabilite);
+Grille* grilleManuelle (Grille *grille);
+void grilleManuelle_v2(Grille *grille);
+int nombreVoisins (Grille* grille, int i, int j);
+int nbVoisinsVivants (Grille *grille, int i, int j);
+Grille* simulation (Grille *grille);
+
+
+
+#endif //AUTOMATES_CELLULAIRES_FONCTIONS_H
